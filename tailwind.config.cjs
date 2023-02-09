93e8457621eeff14ec02f0bc6c05980f6f7d9f69
jssatorres <juansataz.dev@gmail.com>
2023-02-09 11:42:26 +0100

    Feat (config) add eslint and tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,28 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
-  mode:"jt",
+  content: ['./src/**/*.{html,js,jsx}'],
+  mode: 'jt',
   theme: {
     extend: {
       colors: {
-        "deep-blue": "#010026",
-        blue: "#2CBCE9",
-        red: "#DC4492",
-        yellow: "#FDCC49",
-        grey: "#ededed",
-        "dark-grey": "#757575",
-        "opaque-black": "rgba(0,0,0,0.35)",
+        'deep-blue': '#170B33',
+        blue: '#010026',
+        red: '#27496D',
+        yellow: '#FDCC49',
+        purple: '#5F28E0',
+        grey: '#ededed',
+        green: '#18DB32',
+        'dark-grey': '#757575',
+        'opaque-black': 'rgba(0,0,0,0.35)',
+        bluelight: '#DB184C',
+        pink: '#142850',
+        darker: '#27496D',
+        dark: '#27496D',
+        color: '#00909E',
+        light: '#F7EFE5',
+        lighter: '#FFFBF5',
+
       },
       backgroundImage: (theme) => ({
-        "gradient-rainbow":
-          "linear-gradient(81.66deg, #00B5EE 7.21%, #FF45A4 45.05%, #FFBA00 78.07%)",
+        'gradient-rainbow':
+          'linear-gradient(81.66deg, #00B5EE 7.21%, #FF45A4 45.05%, #FFBA00 78.07%)',
 
-        "gradient-rainblue":
-          "linear-gradient(90deg, #24CBFF 14.53%, #FC59FF 69.36%, #FFBD0C 117.73%)",
+        'gradient-rainblue':
+          'linear-gradient(81.66deg, #6d4b27 7.21%, #27496d 45.05%, #27386d 78.07%)',
       }),
       fontFamily: {
-        playfair: ["Playfair Display", "serif"],
-        opensans: ["Open Sans", "sans-serif"],
+        playfair: ['Playfair Display', 'serif'],
+        opensans: ['Open Sans', 'sans-serif'],
       },
       content: {
         brush: "url('./assets/brush.png')",
@@ -32,12 +42,12 @@ module.exports = {
       },
     },
     screens: {
-      xs: "480px",
-      ss: "620px",
-      sm: "768px",
-      md: "1060px",
-      lg: "1200px",
-      xl: "1700px",
+      xs: '480px',
+      ss: '620px',
+      sm: '768px',
+      md: '1060px',
+      lg: '1200px',
+      xl: '1700px',
     },
   },
   plugins: [],
