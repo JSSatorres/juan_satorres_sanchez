5e37501bb0567ed9f914918b01b4e1f0c4d8bdf7
jssatorres <juansataz.dev@gmail.com>
2023-02-09 11:47:40 +0100

    feat (router) createBrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { Router } from "./Router";
+import React from 'react'
+import Router from './Router'
 // import { useState } from 'react'
-// import { Routes, Route, Outlet, Link } from "react-router-dom";
+// import { Routes, Route, Outlet, Link } from 'react-router-dom'
 // import { motion } from 'framer-motion'
 import './assets/styles/App.scss'
+import PositionProvider from './context/positionContext'
 
 function App() {
-  return <Router />;
+  return (
+    <PositionProvider>
+      <Router />
+    </PositionProvider>
+  )
 }
-
 
 export default App
--- src/Router.jsx
+++ src/Router.jsx
@@ -1,40 +1,43 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Layout from './layout/Layout';
-import { HomeFactory } from './pages/home/HomeFactory';
-import NoMatch from './pages/noMatch';
-import AboutMe from './pages/aboutMe';
-import Contact from "./pages/contact";
-import Projects from "./pages/Projects";
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './layout/Layout'
+import { HomeFactory } from './pages/home/HomeFactory'
+import NoMatch from './pages/noMatch'
+import AboutMe from './pages/aboutMe'
+import ContactPage from './pages/contact'
+import ProjectsPage from './pages/projectsPage'
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <Layout />,
-		children: [
-			{
-				path: "/",
-				element:HomeFactory.create(),
-			},
-			{
-				path: "aboutme",
-				element: <AboutMe />,
-			},
-			{
-				path: "contact",
-				element: <Contact />,
-			},
-			{
-				path: "projects",
-				element: <Projects />,
-			},
-			{
-				path: "*",
-				element: <NoMatch />,
-			},
-		],
-	},
-]);
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: HomeFactory.create(),
+      },
+      {
+        path: 'aboutme',
+        element: <AboutMe />,
+      },
+      {
+        path: 'contact',
+        element: <ContactPage />,
+      },
+      {
+        path: 'projects',
+        element: <ProjectsPage />,
+      },
+      {
+        path: '*',
+        element: <NoMatch />,
+      },
+    ],
+  },
+])
 
-export function Router() {
-	return <RouterProvider router={router} />;
+function Router() {
+  return <RouterProvider router={router} />
 }
+
+export default Router
