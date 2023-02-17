a985db0701428aebd3067f92763f5fa7fbdc7345
jssatorres <juansataz.dev@gmail.com>
2023-02-17 21:11:52 +0100

    Fix (Description) add styles clean code

--- src/components/description/Description.jsx
+++ src/components/description/Description.jsx
@@ -1,43 +1,38 @@
 // import SocialMediaIcons from '../components/SocialMediaIcons'
 import React from 'react'
 import { motion } from 'framer-motion'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
+
 import useMediaQuery from '../../hooks/useMediaQuery'
-import imageProfile from '../../assets/profile-image.png'
+import imageProfile from '../../assets/images/profile-image.png'
 import SocialMediaIcons from '../socialMediaIcons'
 
-const Description = ({ setSelectedPage }) => {
+const Description = () => {
   const isAboveLarge = useMediaQuery('(min-width: 1060px)')
   return (
     <section
-      // id='home'
+      id='description'
       className='md:flex md:justify-between md:items-center gap-16 md:h-full py-10'
     >
-      {/* IMAGE SECTION */}
-      <div className='basis-3/5 z-10 mt-16 md:mt-32 flex justify-center md:order-2'>
+      <div className='basis-3/5 mt-12 md:mt-0 flex justify-center md:order-2'>
         {isAboveLarge ? (
           <div
-            className='relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:rounded-t-[200px]
-            before:w-full before:max-w-[400px] md:before:max-w-[600px] before:h-full before:border-2 before:border-blue before:z-[-1]'
+            className='relative ml-10 '
           >
             <img
               alt='profile'
-              className='hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px] rounded-t-[200px]'
+              className='hover:filter hover:saturate-200 transition duration-500  w-full max-w-[400px] md:max-w-[600px] rounded-[20px]'
               src={imageProfile}
             />
           </div>
         ) : (
           <img
             alt='profile'
-            className='z-2 w-full max-w-[400px] md:max-w-[600px] rounded-t-[200px]'
+            className='w-full max-w-[400px] md:max-w-[600px]  rounded-[40px] shadow-xl mb-10'
             src={imageProfile}
           />
         )}
       </div>
-
-      {/* MAIN TEXT */}
-      <div className='z-30 basis-2/5 mt-12 md:mt-32'>
-        {/* HEADINGS */}
+      <div className='z-30 basis-4/5 mt-12 md:mt-20'>
         <motion.div
           initial='hidden'
           whileInView='visible'
@@ -48,7 +43,7 @@ const Description = ({ setSelectedPage }) => {
             visible: { opacity: 1, x: 0 },
           }}
         >
-          <p className='text-6xl font-playfair z-10 text-center md:text-start'>
+          <p className='text-5xl font-playfair text-center md:text-start'>
             Juan Sánchez Satorres
           </p>
           <p className='mt-10 mb-7 text-sm text-center md:text-start'>
@@ -60,31 +55,8 @@ const Description = ({ setSelectedPage }) => {
             technologies.
           </p>
         </motion.div>
-
-        {/* CALL TO ACTIONS */}
         <motion.div
-          className='flex mt-5 justify-center md:justify-start'
-          initial='hidden'
-          whileInView='visible'
-          viewport={{ once: true, amount: 0.5 }}
-          transition={{ delay: 0.2, duration: 0.5 }}
-          variants={{
-            hidden: { opacity: 0, x: -50 },
-            visible: { opacity: 1, x: 0 },
-          }}
-        >
-          <AnchorLink
-            className='bg-gradient-rainblue text-deep-blue rounded-sm py-3 px-7 font-semibold
-              hover:bg-blue hover:text-white transition duration-500'
-            onClick={() => setSelectedPage('contact')}
-            href='#contact'
-          >
-            Contact Me
-          </AnchorLink>
-        </motion.div>
-
-        <motion.div
-          className='flex mt-5 justify-center md:justify-start'
+          className='flex  justify-center md:justify-start'
           initial='hidden'
           whileInView='visible'
           viewport={{ once: true, amount: 0.5 }}
@@ -94,7 +66,9 @@ const Description = ({ setSelectedPage }) => {
             visible: { opacity: 1, x: 0 },
           }}
         >
-          <SocialMediaIcons />
+          <div className='bg-cover bg-center px-3 my-0 rounded-3xl h-12 flex items-center shadow-2xl' style={{ backgroundImage: 'radial-gradient(circle at 50% 40%, #e1e7ec29, #6bb7ee60, #b3c5d39f, #ebf8ffb2)', backdropFilter: 'blur(20px)' }}>
+            <SocialMediaIcons />
+          </div>
         </motion.div>
       </div>
     </section>
