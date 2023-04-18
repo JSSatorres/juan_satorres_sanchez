b68d7c1d65326a84c784085d714d7a713216a460
jssatorres <juansataz.dev@gmail.com>
2023-04-18 18:14:29 +0200

    feat: main Layout

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import { Outlet } from "react-router-dom";
-import Header from '../components/header';
+import { Outlet } from 'react-router-dom'
+import Header from '../components/header'
+import Footer from '../components/footer'
 
-const Layout = () => {
-  return (
-    <div>
-      <Header />
-      <Outlet />
-    </div>
-  )
-}
+const Layout = () => (
+  <div>
+    <Header />
+    <Outlet />
+    <Footer />
+
+  </div>
+)
 
 export default Layout
