0d7988d25ba20df31881419153e3e5ee58055b87
jssatorres <juansataz.dev@gmail.com>
2023-04-18 18:12:14 +0200

    feat: tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   content: ['./src/**/*.{html,js,jsx}'],
   mode: 'jt',
@@ -76,5 +78,31 @@ module.exports = {
       xl: '1700px',
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(({
+      addBase, addComponents, addUtilities, theme,
+    }) => {
+      addBase({
+        h1: {
+          fontSize: theme('fontSize.2xl'),
+        },
+        h2: {
+          fontSize: theme('fontSize.xl'),
+        },
+      })
+      addComponents({
+        '.card': {
+          backgroundColor: theme('colors.white'),
+          borderRadius: theme('borderRadius.lg'),
+          padding: theme('spacing.6'),
+          boxShadow: theme('boxShadow.xl'),
+        },
+      })
+      addUtilities({
+        '.content-auto': {
+          contentVisibility: 'auto',
+        },
+      })
+    }),
+  ],
 }
