82f0673997ff9ef38ec69b6dad4a2032b21f430d
jssatorres <juansataz.dev@gmail.com>
2023-07-06 16:58:26 +0200

    feat: NoMatch page initial

--- src/pages/NoMatch/NoMatch.jsx
+++ src/pages/NoMatch/NoMatch.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 
-const NoMatch = () => {
-  return (
-    <div>
-      no match
-    </div>
-  )
-}
+const NoMatch = () => (
+  <div>
+    no match
+  </div>
+)
 
 export default NoMatch
