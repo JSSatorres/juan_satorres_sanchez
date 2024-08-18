c1c3416f07780c650f6b65fa5b8142a07eccfb1b
JSSatorres <juansataz.dev@gmail.com>
2024-08-18 23:48:47 +0200

    feat: add root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,26 @@
-import type { Metadata } from 'next'
-import './globals.css'
-import { Manrope } from "next/font/google"
-
+import type { Metadata } from "next";
+import "./globals.css";
+import { Manrope } from "next/font/google";
+import NavBar from "@/components/sections/NavBar";
 
 export const metadata: Metadata = {
-  title: 'Photography Portfolio Website',
-  description: 'The best portfolio website',
-}
+  title: "Juan Portfolio",
+  description: "Juan Satorres Sanchez portfolio web developer",
+};
 
-const manrope = Manrope({ subsets: ['latin'], weight: ["400", "500", "600"] })
-
+const manrope = Manrope({ subsets: ["latin"], weight: ["400", "500", "600"] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
-  return (   
+  return (
     <html lang="en">
-      <body  className={manrope.className} >{children}</body>
+      <body className={manrope.className}>
+        <NavBar />
+        <main className="bg-black">{children}</main>
+      </body>
     </html>
-  )
+  );
 }
