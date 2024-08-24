59bb1e148d6b7f3463dce24a98f636c2d371c521
JSSatorres <juansataz.dev@gmail.com>
2024-08-25 01:07:26 +0200

    feat: add main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,30 @@
-import NavBar from "@/components/common/NavBar";
 import AboutSection from "@/components/sections/AboutSection";
-import FaqSection from "@/components/sections/FaqSection";
-import FooterSection from "@/components/sections/FooterSection";
-import HeroSection from "@/components/sections/HeroSection";
-import LargeTextSection from "@/components/sections/LargeTextSection";
-import PortfolioSection from "@/components/sections/PortfolioSection";
-import ServiceSection from "@/components/sections/ServiceSection";
-import TestimonialSection from "@/components/sections/TestimonialSection";
+import ContactSection from "@/components/sections/ContactSection";
+
+// import FaqSection from "@/components/sections/FaqSection";
+// import FooterSection from "@/components/sections/FooterSection";
+// import HeroSection from "@/components/sections/HeroSection";
+// import LargeTextSection from "@/components/sections/LargeTextSection";
+// import PortfolioSection from "@/components/sections/PortfolioSection";
+// import ServiceSection from "@/components/sections/ServiceSection";
+// import TestimonialSection from "@/components/sections/TestimonialSection";
+import { Contact } from "lucide-react";
 
 export default function Home() {
   return (
-    <main className="bg-background">
-      <NavBar />
-      <div className="pt-[10rem] lg:pt-0">
-        <HeroSection />
-        <div className="px-4 md:px-[6rem] flex flex-col gap-[5rem] md:gap-[12.5rem] mt-[5rem] md:mt-[12.5rem]">
-          <AboutSection />
-          <ServiceSection />
-          <PortfolioSection />
-          <FaqSection />
-          <TestimonialSection />
-        </div>
+    <div className="px-4 md:px-[6rem] flex flex-col justify-center">
+      <ContactSection />
+      <AboutSection />
+    </div>
 
-        <LargeTextSection />
-        <FooterSection />
-      </div>
-    </main>
+    // <div className="px-4 md:px-[6rem] flex flex-col gap-[5rem] md:gap-[12.5rem] mt-[5rem] md:mt-[12.5rem]">
+    //   <ServiceSection />
+    //   <PortfolioSection />
+    //   <FaqSection />
+    //   <TestimonialSection />
+
+    // <LargeTextSection />
+    // <FooterSection />
+    // </div>
   );
 }
