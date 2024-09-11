ea0ca62eb5c128887ad852ec360471a9e6b26a06
JSSatorres <juansataz.dev@gmail.com>
2024-09-11 17:00:13 +0200

    feat: delete extra div

--- src/components/sections/AboutSection.tsx
+++ src/components/sections/AboutSection.tsx
@@ -1,17 +1,15 @@
 import React from "react";
-import Header from "../common/Header";
-import { PORTFOLIO_OWNER_FIRSTNAME } from "@/lib/constant";
-import MainButton from "../common/MainButton";
-import { ArrowRight } from "lucide-react";
-import { Separator } from "@/components/ui/separator";
+// import Header from "../common/Header";
+// import { PORTFOLIO_OWNER_FIRSTNAME } from "@/lib/constant";
+// import MainButton from "../common/MainButton";
+// import { ArrowRight } from "lucide-react";
+// import { Separator } from "@/components/ui/separator";
 import AboutCard from "../cards/AboutCard";
-import Image from "next/image";
+// import Image from "next/image";
 
 function AboutSection() {
   return (
-    <section className="grid grid-cols-1 md:grid-cols-2 gap-[1.25rem] mt-32">
-      <div className="flex-grow-1 "></div>
-
+    <section className=" mt-6">
       <AboutCard />
     </section>
   );
