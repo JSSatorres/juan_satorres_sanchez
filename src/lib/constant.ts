0b1f4c275630f121ad8078298d253652688b9feb
JSSatorres <juansataz.dev@gmail.com>
2024-09-12 07:43:36 +0200

    feat: add resume experience

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,2 +1,69 @@
 export const PORTFOLIO_OWNER_FIRSTNAME = "juan";
 export const PORTFOLIO_OWNER_FULLNAME = "Juan Sánchez Satorres";
+
+export const RESUME_INTRODUCTION =
+  "I am a passionate Full-Stack Web Developer with a solid background in industrial design. After a successful career as a technical engineer, I decided to shift my professional trajectory towards software development, specializing in building robust and scalable web applications. With expertise in technologies such as React.js, Next.js, Node.js, TypeScript, and knowledge of Python, I have worked on complex projects that seamlessly integrate frontend and backend. My focus is on optimizing processes and enhancing user experience through innovative and efficient solutions.";
+
+export const RESUME_EXPERIENCE = [
+  {
+    jobTitle: "Full-Stack Web Development",
+    company: "Goblintrader",
+    duration: "Oct 2023 - Present",
+    jobDescription:
+      "Currently developing both frontend and backend solutions using Next.js, TypeScript, Node.js, and Express. Focused on building scalable applications and optimizing user experience.",
+  },
+  {
+    jobTitle: "Frontend Development",
+    company: "Viewnext",
+    duration: "Mar 2023 - Present",
+    jobDescription:
+      "Specializing in frontend development with React.js, Redux, and MaterialUI. Contributing to the creation of high-performance user interfaces and enhancing application functionality.",
+  },
+  {
+    jobTitle: "Software Development",
+    company: "Vocces Lab",
+    duration: "Jun 2022 - Mar 2023",
+    jobDescription:
+      "Designed and implemented an internal logistics application and streamlined company registration processes. Utilized the MERN stack (MongoDB, Express.js, React.js, Node.js), along with Redux, React Query, and MaterialUI, to deliver efficient and user-centric solutions.",
+  },
+  {
+    jobTitle: "Technical Engineering",
+    company: "STULZ TECNIVEL S.L.",
+    duration: "Apr 2018 - Sep 2021",
+    jobDescription:
+      "Led the design and development of air handling units. Managed product design phases, ensuring quality control and adherence to technical specifications. Oversaw production supervision and delivery processes.",
+  },
+  {
+    jobTitle: "Product Development",
+    company: "TECNSIS 3000",
+    duration: "Mar 2017 - Apr 2018",
+    jobDescription:
+      "Engaged in product development and engineering projects, optimizing performance and enhancing technical processes. Contributed to improving product functionality and efficiency.",
+  },
+];
+
+//  Experience: Goblintrader (Oct 2023 -
+//   Present, Part-time, Remote): Currently developing both frontend and
+//   backend solutions using Next.js, TypeScript, Node.js, and Express.
+//   Focused on building scalable applications and optimizing user
+//   experience. Viewnext (Mar 2023 - Present, Remote): Specializing in
+//   frontend development with React.js, Redux, and MaterialUI.
+//   Contributing to the creation of high-performance user interfaces and
+//   enhancing application functionality. Vocces Lab (Jun 2022 - Mar 2023,
+//   Remote): Designed and implemented an internal logistics application
+//   and streamlined company registration processes. Utilized the MERN
+//   stack (MongoDB, Express.js, React.js, Node.js), along with Redux,
+//   React Query, and MaterialUI, to deliver efficient and user-centric
+//   solutions. Technical Engineering Experience: STULZ TECNIVEL S.L. (Apr
+//   2018 - Sep 2021): Led the design and development of air handling
+//   units. Managed product design phases, ensuring quality control and
+//   adherence to technical specifications. Oversaw production supervision
+//   and delivery processes. TECNSIS 3000 (Mar 2017 - Apr 2018): Engaged in
+//   product development and engineering projects, optimizing performance
+//   and enhancing technical processes. Contributed to improving product
+//   functionality and efficiency. Background in Industrial Design:
+//   University of Malaga (Sep 2009 - Jun 2015): Earned a degree in
+//   Technical Industrial Engineering in Industrial Design. This education
+//   laid the groundwork for a strong analytical approach and
+//   problem-solving skills, which have been instrumental in both
+//   engineering and software development roles.
