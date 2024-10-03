8f3da383ebe7da374f172dcfc63c0947c8dc1c24
JSSatorres <juansataz.dev@gmail.com>
2024-10-03 07:55:44 +0200

    feat: navbar wip

--- src/components/sections/NavBar.tsx
+++ src/components/sections/NavBar.tsx
@@ -12,28 +12,51 @@ function NavBar() {
     setMenu(!menu);
   };
 
-  const pagesLink =[
-    { href: "/", text: "Home" },
+  const pagesLink = [
+    { href: "/experience", text: "Experience" },
     { href: "/projects", text: "Projects" },
     { href: "/contact", text: "Contact" },
     // { href: "/about", text: "About" },
     // { href: "/services", text: "Services" },
-  ]
+  ];
+
+  // <header className="max-w-7xl mx-auto px-4 py-8">
+  //   <nav className="flex justify-between items-center">
+  //     <h1 className="text-2xl font-bold">Adams</h1>
+  //     <ul className="flex space-x-4">
+  //       <li>
+  //         <a href="#">Home</a>
+  //       </li>
+  //       <li>
+  //         <a href="#">Portfolio</a>
+  //       </li>
+  //       <li>
+  //         <a href="#">Testimonials</a>
+  //       </li>
+  //     </ul>
+  //   </nav>
+  // </header>;
 
   return (
-    <nav className="md:sticky md:top-0  bg-background md:shadow-none z-[10] p-4">
-      <ul className="flex justify-center gap-20 items-center ">
-        {pagesLink.map((item) => (
-          <li key={item.href}>
-            <Link href={item.href} className="text-white hover:text-gray-400 text-2xl md:text-4xl font-extrabold uppercase">
-              {item.text}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <header className="max-w-7xl mx-auto px-4 py-8 sticky top-0">
+      {/* <nav className="md:sticky md:top-0  bg-background md:shadow-none z-[10] p-4"> */}
+      <nav className="flex justify-between items-center">
+        <h1 className="text-white text-2xl font-bold">Home</h1>
+        <ul className="flex space-x-4">
+          {pagesLink.map((item) => (
+            <li key={item.href}>
+              <Link
+                href={item.href}
+                className="text-white hover:text-gray-400 text-2xl md:text-4xl font-extrabold uppercase"
+              >
+                {item.text}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
   );
 }
 
 export default NavBar;
-
