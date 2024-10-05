6b8c9fcc8d2483035433d22e549115dd49a778ee
JSSatorres <juansataz.dev@gmail.com>
2024-10-05 10:57:42 +0200

    feat: add global styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,52 +1,32 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
-  prefix: "",
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     extend: {
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
       colors: {
-        primary: "#4A2CED",
-        background: "#070708",
-        customGray: "#62646C",
-        customGrayAlt: "#797C86",
-        customGrayAlt2: "#AFB0B6",
-        customGray90: "#E4E4E6",
-        lightGray: "#CACACE",
-        lightPurple: "#D6D0FB",
-        superGray: "#2F2F37",
-        lightDark: "#1C1C21"
-      }
+        primary: "#4A2C80", // Color primario cambiar
+        background: "#111827", // Gris 900
+        backgroundLight: "#1f2937", // Gris 800
+        customGrayAlt: "#797C86", // Otro gris
+        customGrayAlt2: "#AFB0B6", // Un tercer gris
+        customGray90: "#E4E4E6", // Gris claro
+        lightGray: "#CACACE", // Gris claro
+        lightPurple: "#D6D0FB", // Púrpura claro
+        superGray: "#2F2F37", // Gris oscuro
+        lightDark: "#1C1C21", // Otro color oscuro
+        textColorIm: "#FFD700", // Color para "I’m"
+        textColorName: "#00BFFF", // Color para el nombre
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
