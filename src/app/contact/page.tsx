c6f6ed0f3826cd7ba2286c5e8c7d65fb9f272ed0
JSSatorres <juansataz.dev@gmail.com>
2024-10-07 18:26:16 +0200

    feat: extract yup definitions

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,29 +1,10 @@
 "use client";
+
 import React from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
-
-interface FormInput {
-  name: string;
-  email: string;
-  message: string;
-}
-
-const schema = yup.object().shape({
-  name: yup
-    .string()
-    .required("Name is required")
-    .min(2, "Name must be at least 2 characters"),
-  email: yup
-    .string()
-    .required("Email is required")
-    .email("Must be a valid email"),
-  message: yup
-    .string()
-    .required("Message is required")
-    .min(10, "Message must be at least 10 characters"),
-});
+import { contactSchema } from "./validationSchema";
+import { FormInput } from "@/lib/definitions";
 
 const Contact = () => {
   const {
@@ -32,7 +13,7 @@ const Contact = () => {
     formState: { errors, isSubmitting },
     reset,
   } = useForm<FormInput>({
-    resolver: yupResolver(schema),
+    resolver: yupResolver(contactSchema),
   });
 
   const onSubmit: SubmitHandler<FormInput> = async (data) => {
@@ -46,7 +27,7 @@ const Contact = () => {
       });
 
       if (res.ok) {
-        reset(); // Reset form after successful submission
+        reset();
       } else {
         console.error("Failed to send");
       }
@@ -56,71 +37,75 @@ const Contact = () => {
   };
 
   return (
-    <div className="max-w-2xl mx-auto bg-gray-800 text-white p-8 rounded-lg shadow-md mt-10">
-      <h1 className="text-3xl font-bold mb-6 text-center">Contact Me</h1>
-      <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
-        <div>
-          <label className="block text-sm font-medium text-gray-400 mb-1">
-            Name
-          </label>
-          <input
-            type="text"
-            {...register("name")}
-            className={`w-full p-3 bg-gray-700 text-white rounded-lg focus:outline-none focus:ring-2 ${
-              errors.name ? "ring-red-500" : "ring-yellow-400"
-            }`}
-            placeholder="Enter your name"
-          />
-          {errors.name && (
-            <p className="text-red-500 text-sm mt-2">{errors.name.message}</p>
-          )}
-        </div>
+    <main className="min-h-screen bg-background text-white px-4 py-16 overflow-x-hidden">
+      <div className="max-w-2xl mx-auto bg-gray-800 p-8 rounded-lg shadow-xl mt-10">
+        <h1 className="text-3xl font-bold mb-6 text-center">Contact Me</h1>
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+          <div>
+            <label className="block text-sm font-medium text-gray-400 mb-1">
+              Name
+            </label>
+            <input
+              type="text"
+              {...register("name")}
+              className={`w-full p-3 bg-backgroundLight text-white rounded-lg border-2 ${
+                errors.name ? "border-red-500" : "border-yellow-400"
+              } focus:outline-none focus:ring-2 focus:ring-yellow-500`}
+              placeholder="Enter your name"
+            />
+            {errors.name && (
+              <p className="text-red-500 text-sm mt-2">{errors.name.message}</p>
+            )}
+          </div>
 
-        <div>
-          <label className="block text-sm font-medium text-gray-400 mb-1">
-            Email
-          </label>
-          <input
-            type="email"
-            {...register("email")}
-            className={`w-full p-3 bg-gray-700 text-white rounded-lg focus:outline-none focus:ring-2 ${
-              errors.email ? "ring-red-500" : "ring-yellow-400"
-            }`}
-            placeholder="Enter your email"
-          />
-          {errors.email && (
-            <p className="text-red-500 text-sm mt-2">{errors.email.message}</p>
-          )}
-        </div>
+          <div>
+            <label className="block text-sm font-medium text-gray-400 mb-1">
+              Email
+            </label>
+            <input
+              type="email"
+              {...register("email")}
+              className={`w-full p-3 bg-backgroundLight text-white rounded-lg border-2 ${
+                errors.email ? "border-red-500" : "border-yellow-400"
+              } focus:outline-none focus:ring-2 focus:ring-yellow-500`}
+              placeholder="Enter your email"
+            />
+            {errors.email && (
+              <p className="text-red-500 text-sm mt-2">
+                {errors.email.message}
+              </p>
+            )}
+          </div>
 
-        <div>
-          <label className="block text-sm font-medium text-gray-400 mb-1">
-            Message
-          </label>
-          <textarea
-            {...register("message")}
-            className={`w-full p-3 bg-gray-700 text-white rounded-lg focus:outline-none focus:ring-2 ${
-              errors.message ? "ring-red-500" : "ring-yellow-400"
-            }`}
-            placeholder="Enter your message"
-            rows={5}
-          />
-          {errors.message && (
-            <p className="text-red-500 text-sm mt-2">
-              {errors.message.message}
-            </p>
-          )}
-        </div>
+          <div>
+            <label className="block text-sm font-medium text-gray-400 mb-1">
+              Message
+            </label>
+            <textarea
+              {...register("message")}
+              className={`w-full p-3 bg-backgroundLight text-white rounded-lg border-2 ${
+                errors.message ? "border-red-500" : "border-yellow-400"
+              } focus:outline-none focus:ring-2 focus:ring-yellow-500`}
+              placeholder="Enter your message"
+              rows={5}
+            />
+            {errors.message && (
+              <p className="text-red-500 text-sm mt-2">
+                {errors.message.message}
+              </p>
+            )}
+          </div>
 
-        <button
-          type="submit"
-          className="w-full bg-yellow-400 hover:bg-yellow-500 text-black p-3 rounded-lg font-semibold transition-all ease-in-out duration-300"
-          disabled={isSubmitting}
-        >
-          {isSubmitting ? "Sending..." : "Send Message"}
-        </button>
-      </form>
-    </div>
+          <button
+            type="submit"
+            className="w-full bg-yellow-400 hover:bg-yellow-500 text-black p-3 rounded-lg font-semibold transition-all ease-in-out duration-300"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "Sending..." : "Send Message"}
+          </button>
+        </form>
+      </div>
+    </main>
   );
 };
 
