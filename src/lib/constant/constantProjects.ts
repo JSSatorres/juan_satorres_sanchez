b97505ee2ea47279ef354a05a8986ae8c74f88c3
JSSatorres <juansataz.dev@gmail.com>
2024-10-12 12:31:41 +0200

    feat: add project constant

---
+++ src/lib/constant/constantProjects.ts
@@ -0,0 +1,46 @@
+// src/lib/constant/projects.js
+export const PROJECTS = [
+  {
+    id: 1,
+    name: "Project One",
+    image: "/images/project1.png",
+    technologies: ["React", "Node.js"],
+    url: "https://example.com/project-one", // URL del proyecto
+  },
+  {
+    id: 2,
+    name: "Project Two",
+    image: "/images/project2.png",
+    technologies: ["Vue", "Firebase"],
+    url: "https://example.com/project-two",
+  },
+  {
+    id: 3,
+    name: "Project Three",
+    image: "/images/project3.png",
+    technologies: ["Next.js", "GraphQL"],
+    url: "https://example.com/project-three",
+  },
+  {
+    id: 4,
+    name: "Project Four",
+    image: "/images/project4.png",
+    technologies: ["Angular", "Express"],
+    url: "https://example.com/project-four",
+  },
+  {
+    id: 5,
+    name: "Project Five",
+    image: "/images/project5.png",
+    technologies: ["HTML", "CSS", "JavaScript"],
+    url: "https://example.com/project-five",
+  },
+  {
+    id: 6,
+    name: "Project Six",
+    image: "/images/project6.png",
+    technologies: ["Django", "Python"],
+    url: "https://example.com/project-six",
+  },
+  // Añadir más proyectos según sea necesario
+];
