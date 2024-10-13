c06102df6fd1e354e653c416ea1a0765d2a22f6c
JSSatorres <juansataz.dev@gmail.com>
2024-10-13 19:21:11 +0200

    feat: add project constant

---
+++ src/lib/constant/projectsConstant.ts
@@ -0,0 +1,111 @@
+// import vocces from "@/assets/images/vocces.png";
+// import assembler from "@/assets/images/assembler.png";
+// import stulz from "@/assets/images/stulz.png";
+// import tecnesis from "@/assets/images/tecnesis.png";
+
+export const workExperience = [
+  {
+    company: "Goblintrader",
+    jobTitle: "Full stack web developer",
+    jobDescription: "Full-stack developing projects",
+    duration: "November 2024 - at present",
+    // img: vocces,
+    projects: [
+      {
+        projectTitle: "Cleint Dashboard",
+        technologies: "VueJs, NodeJs,  Pinia, SQL, Sass",
+      },
+      {
+        projectTitle: "Cleint Survey",
+        technologies: "ReactJs, NodeJs,  Redux-toolkit, SQL, Sass",
+      },
+    ],
+  },
+  {
+    company: "Viewnext",
+    jobTitle: "Full stack web developer",
+    jobDescription: "Full-stack developing projects",
+    duration: "June 2023 - at present",
+    // img: vocces,
+    projects: [
+      {
+        projectTitle: "Cleint Dashboard",
+        technologies: "VueJs, NodeJs,  Pinia, SQL, Sass",
+      },
+      {
+        projectTitle: "Cleint Survey",
+        technologies: "ReactJs, NodeJs,  Redux-toolkit, SQL, Sass",
+      },
+    ],
+  },
+  {
+    company: "Vocces Lab",
+    jobTitle: "Full stack web developer",
+    jobDescription: "Full-stack developing projects",
+    duration: "June 2022 - at present",
+    // img: vocces,
+    projects: [
+      {
+        projectTitle: "Cleint Dashboard",
+        technologies: "VueJs, NodeJs,  Pinia, SQL, Sass",
+      },
+      {
+        projectTitle: "Cleint Survey",
+        technologies: "ReactJs, NodeJs,  Redux-toolkit, SQL, Sass",
+      },
+    ],
+  },
+  {
+    company: "Assembler Institute",
+    jobTitle: "Full stack web developer",
+    jobDescription: "Full-stack developing projects",
+    duration: "September 2021 - May 2022",
+    // img: assembler,
+    projects: [
+      {
+        projectTitle: "Clon Spotify",
+        technologies:
+          "ReactJs, Redux-toolkit, NodeJs, Express, Mongoose, Mongo, Sass",
+      },
+      {
+        projectTitle: "real estate portal",
+        technologies: "ReactJs, NodeJs,  Redux-toolkit, SQL, Sass",
+      },
+    ],
+  },
+  {
+    company: "Stulz Tecnivel S.L,",
+    jobTitle: "Technichal engineer",
+    jobDescription:
+      "Design and development of air handling units as team leader",
+    duration: "April 2018 - September 2021",
+    // img: stulz,
+    projects: [
+      {
+        projectTitle: "Cleint Dashboard",
+        technologies: "VueJs, NodeJs,  Pinia, SQL, Sass",
+      },
+      {
+        projectTitle: "Cleint Survey",
+        technologies: "ReactJs, NodeJs,  Redux-toolkit, SQL, Sass",
+      },
+    ],
+  },
+  {
+    company: "Tecnesis 3000",
+    jobTitle: "Technichal engineer",
+    jobDescription: "Product design phase control manager",
+    duration: "February 2017 - April 2018",
+    // img: tecnesis,
+    projects: [
+      {
+        projectTitle: "Cleint Dashboard",
+        technologies: "VueJs, NodeJs,  Pinia, SQL, Sass",
+      },
+      {
+        projectTitle: "Cleint Survey",
+        technologies: "ReactJs, NodeJs,  Redux-toolkit, SQL, Sass",
+      },
+    ],
+  },
+];
