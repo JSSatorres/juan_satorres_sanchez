dff72ff4d5d9142489d6aa644bc6caa7174855a3
JSSatorres <juansataz.dev@gmail.com>
2024-10-16 18:06:42 +0200

    feat: footer component

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -1,28 +1,28 @@
+import { GITHUB_URL, LINKEDIN_URL } from "@/lib/constant/constantHomePage";
 import React from "react";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-gray-800 text-white py-4">
+    <footer className="bg-secondBackgrond text-main py-4">
       <div className="max-w-7xl mx-auto px-4 flex flex-col items-center">
         <p className="text-center mb-2">
-          &copy; {new Date().getFullYear()} Juan Sánchez Satorres. All rights
-          reserved.
+          &copy; {new Date().getFullYear()} Juan Satorres Sánchez
         </p>
         <div className="flex space-x-4">
           <a
-            href="https://github.com/your-username"
+            href={GITHUB_URL}
             target="_blank"
             rel="noopener noreferrer"
-            className="hover:text-green-400 transition duration-200"
+            className="hover:text-primary transition duration-200"
           >
             <FaGithub size={24} />
           </a>
           <a
-            href="https://www.linkedin.com/in/your-username"
+            href={LINKEDIN_URL}
             target="_blank"
             rel="noopener noreferrer"
-            className="hover:text-green-400 transition duration-200"
+            className="text-main hover:text-primary transition duration-200"
           >
             <FaLinkedin size={24} />
           </a>
