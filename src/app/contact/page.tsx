dc2ab66b63740edaf6aadf4da2b44001d804019b
JSSatorres <juansataz.dev@gmail.com>
2024-10-16 17:59:38 +0200

    feat: contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -37,60 +37,58 @@ const Contact = () => {
   };
 
   return (
-    <main className="min-h-screen bg-background text-white px-4 py-16 overflow-x-hidden">
-      <div className="max-w-2xl mx-auto bg-gray-800 p-8 rounded-lg shadow-xl mt-10">
+    <main className="min-h-screen bg-background text-main px-4 py-16 overflow-x-hidden">
+      <div className="max-w-2xl mx-auto bg-secondBackgrond p-8 rounded-lg shadow-xl mt-10">
         <h1 className="text-3xl font-bold mb-6 text-center">Contact Me</h1>
         <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
           <div>
-            <label className="block text-sm font-medium text-gray-400 mb-1">
+            <label className="block text-sm font-medium text-black mb-1">
               Name
             </label>
             <input
               type="text"
               {...register("name")}
-              className={`w-full p-3 bg-backgroundLight text-white rounded-lg border-2 ${
-                errors.name ? "border-red-500" : "border-yellow-400"
-              } focus:outline-none focus:ring-2 focus:ring-yellow-500`}
+              className={`w-full p-3 bg-backgroundLight text-black rounded-lg border-2 ${
+                errors.name ? "border-error" : "border-primary"
+              } focus:outline-none focus:ring-2 focus:ring-darkPrimary`}
               placeholder="Enter your name"
             />
             {errors.name && (
-              <p className="text-red-500 text-sm mt-2">{errors.name.message}</p>
+              <p className="text-error text-sm mt-2">{errors.name.message}</p>
             )}
           </div>
 
           <div>
-            <label className="block text-sm font-medium text-gray-400 mb-1">
+            <label className="block text-sm font-medium text-black mb-1">
               Email
             </label>
             <input
               type="email"
               {...register("email")}
-              className={`w-full p-3 bg-backgroundLight text-white rounded-lg border-2 ${
-                errors.email ? "border-red-500" : "border-yellow-400"
-              } focus:outline-none focus:ring-2 focus:ring-yellow-500`}
+              className={`w-full p-3 bg-backgroundLight text-black rounded-lg border-2 ${
+                errors.email ? "border-error" : "border-primary"
+              } focus:outline-none focus:ring-2 focus:ring-darkPrimary`}
               placeholder="Enter your email"
             />
             {errors.email && (
-              <p className="text-red-500 text-sm mt-2">
-                {errors.email.message}
-              </p>
+              <p className="text-error text-sm mt-2">{errors.email.message}</p>
             )}
           </div>
 
           <div>
-            <label className="block text-sm font-medium text-gray-400 mb-1">
+            <label className="block text-sm font-medium text-black mb-1">
               Message
             </label>
             <textarea
               {...register("message")}
-              className={`w-full p-3 bg-backgroundLight text-white rounded-lg border-2 ${
-                errors.message ? "border-red-500" : "border-yellow-400"
-              } focus:outline-none focus:ring-2 focus:ring-yellow-500`}
+              className={`w-full p-3 bg-backgroundLight text-black rounded-lg border-2 ${
+                errors.message ? "border-error" : "border-primary"
+              } focus:outline-none focus:ring-2 focus:ring-darkPrimary`}
               placeholder="Enter your message"
               rows={5}
             />
             {errors.message && (
-              <p className="text-red-500 text-sm mt-2">
+              <p className="text-error text-sm mt-2">
                 {errors.message.message}
               </p>
             )}
@@ -98,7 +96,7 @@ const Contact = () => {
 
           <button
             type="submit"
-            className="w-full bg-yellow-400 hover:bg-yellow-500 text-black p-3 rounded-lg font-semibold transition-all ease-in-out duration-300"
+            className="w-full bg-main hover:bg-primary text-primary p-3 hover:text-main rounded-lg font-semibold transition-all ease-in-out duration-300"
             disabled={isSubmitting}
           >
             {isSubmitting ? "Sending..." : "Send Message"}
