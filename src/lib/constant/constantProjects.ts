6ec0ebafbc4b5ebdc3caf86a505683cb3eb80b06
JSSatorres <juansataz.dev@gmail.com>
2024-10-17 08:33:58 +0200

    feat: rename atribute in project constant

--- src/lib/constant/constantProjects.ts
+++ src/lib/constant/constantProjects.ts
@@ -2,42 +2,42 @@
 export const PROJECTS = [
   {
     id: 1,
-    name: "Project One",
+    projectTitle: "Project One",
     image: "/images/project1.png",
     technologies: ["React", "Node.js"],
     url: "https://example.com/project-one", // URL del proyecto
   },
   {
     id: 2,
-    name: "Project Two",
+    projectTitle: "Project Two",
     image: "/images/project2.png",
     technologies: ["Vue", "Firebase"],
     url: "https://example.com/project-two",
   },
   {
     id: 3,
-    name: "Project Three",
+    projectTitle: "Project Three",
     image: "/images/project3.png",
     technologies: ["Next.js", "GraphQL"],
     url: "https://example.com/project-three",
   },
   {
     id: 4,
-    name: "Project Four",
+    projectTitle: "Project Four",
     image: "/images/project4.png",
     technologies: ["Angular", "Express"],
     url: "https://example.com/project-four",
   },
   {
     id: 5,
-    name: "Project Five",
+    projectTitle: "Project Five",
     image: "/images/project5.png",
     technologies: ["HTML", "CSS", "JavaScript"],
     url: "https://example.com/project-five",
   },
   {
     id: 6,
-    name: "Project Six",
+    projectTitle: "Project Six",
     image: "/images/project6.png",
     technologies: ["Django", "Python"],
     url: "https://example.com/project-six",
