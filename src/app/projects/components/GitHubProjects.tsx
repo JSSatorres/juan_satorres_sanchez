4f552f6184ffdbe11de7c1ae6d788f99ce672820
JSSatorres <juansataz.dev@gmail.com>
2024-12-02 07:11:50 +0100

    refactor: improve code formatting and structure in GitHubProjects component

--- src/app/projects/components/GitHubProjects.tsx
+++ src/app/projects/components/GitHubProjects.tsx
@@ -1,5 +1,5 @@
-import { GitHubRepo } from '@/lib/definitions';
-import ProjectCard from '@/components/cards/ProjectCard';
+import { GitHubRepo } from "@/lib/definitions";
+import ProjectCard from "@/components/cards/ProjectCard";
 
 type GitHubProjectProps = {
   repos: GitHubRepo[];
@@ -8,40 +8,46 @@ type GitHubProjectProps = {
   onTechChange: (tech: string) => void;
 };
 
-const GitHubProject = ({ repos, selectedTech, topTechnologies, onTechChange }: GitHubProjectProps) => {
+const GitHubProject = ({
+  repos,
+  selectedTech,
+  topTechnologies,
+  onTechChange,
+}: GitHubProjectProps) => {
   const filteredRepos = repos.filter((repo) =>
     selectedTech ? repo.language === selectedTech : true
   );
 
   return (
     <>
-      <div className="mb-8 w-full flex justify-center">
+      <div className="mb-8 w-full flex flex-col items-center justify-center">
+        <hr className="w-full mb-5 mt-5 " />
+        <h2 className="text-3xl font-bold mb-5 mt-5">All Repositories</h2>
         <select
-          className="w-full max-w-md p-2 rounded-lg bg-backgroundLight text-main"
+          className="w-full max-w-md p-2 rounded-lg bg-backgroundLight text-main mt-5"
           value={selectedTech}
           onChange={(e) => onTechChange(e.target.value)}
         >
           <option value="">All Technologies</option>
           {topTechnologies.map((tech) => (
-            <option key={tech} value={tech}>{tech}</option>
+            <option key={tech} value={tech}>
+              {tech}
+            </option>
           ))}
         </select>
       </div>
 
-      <div className="flex flex-col items-center justify-center text-center"> 
+      <div className="flex flex-col items-center justify-center text-center">
         <section className="flex flex-col items-center justify-center text-center">
           <div className="w-4/5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            {filteredRepos.map((repo) => (              
-                <ProjectCard repo={repo} key={repo.id} />
+            {filteredRepos.map((repo) => (
+              <ProjectCard repo={repo} key={repo.id} />
             ))}
           </div>
-        </section>    
-
+        </section>
       </div>
     </>
   );
 };
 
 export default GitHubProject;
-
-
