a6cb225eff6451e1a1cee06213a611dd6a91d39e
JSSatorres <juansataz.dev@gmail.com>
2024-12-20 07:03:47 +0100

    refactor: improve layout and styling in ExperienceCard for enhanced readability and presentation

--- src/components/cards/ExperienceCard.tsx
+++ src/components/cards/ExperienceCard.tsx
@@ -12,26 +12,31 @@ const ProjectCard = ({ work }: { work: WorkExperience }) => {
   console.log(work);
 
   return (
-    <Card className="bg-secondBackgrond flex flex-col justify-center items-center p-4 m-9 w-5/6 card_fade_in">
-      <CardHeader className="flex flex-col justify-center items-center text-center">
-        <CardTitle className="text-main text-4xl">{work?.company}</CardTitle>
-        <CardDescription className="text-main text-xl mt-2">
+    <Card className="bg-secondBackgrond flex flex-col p-4 m-9 w-5/6 card_fade_in">
+      <CardHeader className="text-main flex flex-col justify-center items-center text-center">
+        <CardTitle className="text-main text-4xl mb-4">
+          {work?.company}
+        </CardTitle>
+        <CardDescription className="text-primary  text-xl mt-5">
           {work?.jobTitle}
         </CardDescription>
-        <CardDescription className="text-main text-xl mt-1">
+        <CardDescription className="text-primary  text-xl mt-1">
           {work?.duration}
         </CardDescription>
       </CardHeader>
-      <CardContent className="text-main text-justify">
+      <CardContent className="text-justify w-full">
         <p>{work?.jobDescription}</p>
       </CardContent>
       {work?.projects.map((project) => (
-        <CardContent key={project?.projectTitle} className="text-main mt-4">
-          <CardTitle className="text-center text-2xl mb-2">
+        <CardContent
+          key={project?.projectTitle}
+          className="text-main mt-4 w-full"
+        >
+          <CardTitle className="text-left text-2xl mb-2">
             {project?.projectTitle}
           </CardTitle>
           <CardDescription className="mb-2">
-            <strong>Technologies:</strong>{" "}
+            <strong>Technologies:</strong>
             <span>{project.technologies.join(", ")}</span>
           </CardDescription>
           <CardDescription className="mb-2">
@@ -49,23 +54,8 @@ const ProjectCard = ({ work }: { work: WorkExperience }) => {
               />
             </div>
           )}
-          {project.url && (
-            <div className="text-center mt-4">
-              <a
-                href={project.url}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-500 underline"
-              >
-                Project Link
-              </a>
-            </div>
-          )}
         </CardContent>
       ))}
-      <CardFooter className="text-center mt-4">
-        <p>Card Footer</p>
-      </CardFooter>
     </Card>
   );
 };
