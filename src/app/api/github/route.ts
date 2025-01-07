3eed054385f3859f45595e2d09ca0ed4ec276747
JSSatorres <juansataz.dev@gmail.com>
2025-01-07 17:09:32 +0100

    refactor: remove console.log statements for cleaner code

--- src/app/api/github/route.ts
+++ src/app/api/github/route.ts
@@ -4,9 +4,6 @@ export async function GET() {
   const username = process.env.NEXT_PUBLIC_GITHUB_USERNAME;
   const token = process.env.NEXT_PUBLIC_GITHUB_TOKEN;
 
-console.log( "GITHUB_USERNAME aaaaaaa:", process.env.NEXT_PUBLIC_GITHUB_USERNAME, "GITHUB_TOKEN:aaaaaaa", process.env.NEXT_PUBLIC_GITHUB_TOKEN)
-
-
   const response = await fetch(
     `https://api.github.com/users/${username}/repos`,
     {
