d17088fad4cda056e0ceac720feed10e19ed3fb2
JSSatorres <juansataz.dev@gmail.com>
2025-01-13 10:22:59 +0100

    refactor: streamline Tailwind CSS configuration by removing commented code and adding new 3D shadow settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,44 +1,3 @@
-// import type { Config } from "tailwindcss";
-
-// const config = {
-//   darkMode: ["class"],
-//   content: [
-//     "./pages/**/*.{ts,tsx}",
-//     "./components/**/*.{ts,tsx}",
-//     "./app/**/*.{ts,tsx}",
-//     "./src/**/*.{ts,tsx}",
-//   ],
-//   theme: {
-//     extend: {
-//       colors: {
-//         main: "#1F375B", // Azul oscuro
-//         primary: "#05C7F2", // Color azuk claro
-//         darkPrimary: "#049AC0",
-//         secondary: "#F7AF6D", // Amarillo"
-//         success: "#28A745",
-//         error: "#FF0D06",
-//         warning: "#FFC107",
-//         info: "#17A2B8",
-//         tertiary: "#6F42C1",
-//         Darktxt: "black",
-
-//         background: "#EAF2FB", // Gris muy claro
-//         secondBackgrond: "#D5E4F6", //
-//         backgroundLight: "#E0EAF8", // Gris 800
-//         customGrayAlt: "#797C86", // Otro gris
-//         customGrayAlt2: "#AFB0B6", // Un tercer gris
-//         customGray90: "#E4E4E6", // Gris claro
-//         lightGray: "#CACACE", // Gris claro
-//         lightPurple: "#D6D0FB", // Púrpura claro
-//         superGray: "#2F2F37", // Gris oscuro
-//         lightDark: "#1C1C21", // Otro color oscuro
-//       },
-//     },
-//   },
-//   plugins: [require("tailwindcss-animate")],
-// } satisfies Config;
-
-// export default config;
 import type { Config } from "tailwindcss";
 
 const config = {
@@ -64,8 +23,10 @@ const config = {
         textSecondary: "#6C6A6D", // Texto secundario
       },
       boxShadow: {
-        subtle: "0 4px 15px rgba(178, 165, 149, 0.3)", // Sombra sutil
-        deep: "0 6px 20px rgba(178, 165, 149, 0.5)", // Sombra profunda
+        "3d-soft":
+          "6px 6px 12px rgba(178, 165, 149, 0.3), -4px -4px 8px rgba(255, 255, 255, 0.8)",
+        "3d-deep":
+          "8px 8px 16px rgba(178, 165, 149, 0.5), -6px -6px 12px rgba(255, 255, 255, 0.9)",
       },
       borderRadius: {
         card: "20px", // Bordes redondeados de tarjetas
