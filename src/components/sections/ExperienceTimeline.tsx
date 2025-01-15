4c5d5d2300c7fdf8cd86dc01dd147d567666031f
JSSatorres <juansataz.dev@gmail.com>
2025-01-15 10:20:53 +0100

    refactor: update ExperienceTimeline component styles for improved visual clarity

--- src/components/sections/ExperienceTimeline.tsx
+++ src/components/sections/ExperienceTimeline.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { workExperience } from "@/lib/constant/projectsConstant"; // Importamos la constante directamente
+import { workExperience } from "@/lib/constant/projectsConstant";
 
 interface YearTimelineProps {
   activeIndex: number;
@@ -13,7 +13,7 @@ const ExperienceTimeline: React.FC<YearTimelineProps> = ({
   getYearFromDate,
 }) => {
   return (
-    <div className="md:fixed md:top-50 bg-secondBackgrond p-5 flex md:flex-col flex-row md:items-center items-center justify-center">
+    <div className="md:fixed md:top-50 bg-secondBackgrond p-5 flex md:flex-col flex-row md:items-center items-center justify-center card">
       <ul className="flex md:flex-col flex-row items-center md:space-y-8 space-x-8 md:space-x-0 relative p-5">
         {workExperience.map((work, index) => (
           <li
@@ -21,11 +21,12 @@ const ExperienceTimeline: React.FC<YearTimelineProps> = ({
             className={`cursor-pointer text-xl font-bold transition duration-200 text-main relative flex md:flex-row flex-col items-center`}
             onClick={() => handleScrollTo(index)}
           >
-            {/* Puntos de la línea */}
             <div className="relative flex items-center">
               <span
                 className={`w-4 h-4 rounded-full z-10 ${
-                  activeIndex === index ? "bg-green-400" : "bg-main"
+                  activeIndex === index
+                    ? "bg-primary"
+                    : "bg-white border-2 border-primary"
                 } hover:bg-opacity-75`}
               ></span>
               <span className="text-sm ml-4 md:ml-2">
@@ -33,7 +34,6 @@ const ExperienceTimeline: React.FC<YearTimelineProps> = ({
               </span>
             </div>
 
-            {/* Línea que conecta los puntos */}
             {index < workExperience.length - 1 && (
               <div className="absolute md:block hidden left-2 transform translate-x-1/2 top-6 h-full border-l-2 border-gray-600 z-0"></div>
             )}
