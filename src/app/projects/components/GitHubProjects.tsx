b34973f2a32e09ac874c43f914930747be5bc59d
JSSatorres <juansataz.dev@gmail.com>
2025-01-16 09:21:39 +0100

    refactor: enhance GitHubProjects component layout and styling for better presentation

--- src/app/projects/components/GitHubProjects.tsx
+++ src/app/projects/components/GitHubProjects.tsx
@@ -1,5 +1,4 @@
 import { GitHubRepo } from "@/lib/definitions";
-import ProjectCard from "@/components/cards/ProjectCard";
 
 type GitHubProjectProps = {
   repos: GitHubRepo[];
@@ -14,17 +13,15 @@ const GitHubProject = ({
   topTechnologies,
   onTechChange,
 }: GitHubProjectProps) => {
-  const filteredRepos = repos.filter((repo) =>
-    selectedTech ? repo.language === selectedTech : true
-  );
-
   return (
     <>
       <div className="mb-8 w-full flex flex-col items-center justify-center">
-        <hr className="w-full mb-5 mt-5 " />
-        <h2 className="text-3xl font-bold mb-5 mt-5">All Repositories</h2>
+        <hr className="w-full mb-5 mt-5 border-border" />
+        <h2 className="text-3xl font-bold mb-5 mt-5 text-main">
+          All Repositories
+        </h2>
         <select
-          className="w-full max-w-md p-2 rounded-lg bg-backgroundLight text-main mt-5"
+          className="w-full max-w-md  card text-main shadow-3d-soft hover:shadow-3d-deep border transition-all"
           value={selectedTech}
           onChange={(e) => onTechChange(e.target.value)}
         >
@@ -39,9 +36,26 @@ const GitHubProject = ({
 
       <div className="flex flex-col items-center justify-center text-center">
         <section className="flex flex-col items-center justify-center text-center">
-          <div className="w-4/5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            {filteredRepos.map((repo) => (
-              <ProjectCard repo={repo} key={repo.id} />
+          <div className="w-4/5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-14">
+            {repos.map((repo) => (
+              <div key={repo.id} className="flex flex-col justify-between card">
+                <h3 className="text-xl font-bold text-main">{repo.name}</h3>
+                <p className="text-textSecondary mt-2">{repo.description}</p>
+                <p className="text-sm text-gray-500 mt-1">
+                  <strong>Language:</strong> {repo.language || "Not specified"}
+                </p>
+                <p className="text-sm text-gray-500 mt-1">
+                  <strong>Created on:</strong> {repo.created_at}
+                </p>
+                <a
+                  href={repo.html_url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="mt-3 text-primary hover:underline"
+                >
+                  View on GitHub
+                </a>
+              </div>
             ))}
           </div>
         </section>
