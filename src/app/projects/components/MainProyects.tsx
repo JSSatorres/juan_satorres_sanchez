db5824113b030f90896c724356c67094be93dbc2
JSSatorres <juansataz.dev@gmail.com>
2025-01-16 09:21:43 +0100

    refactor: update MainProyects component layout and styling for improved presentation

--- src/app/projects/components/MainProyects.tsx
+++ src/app/projects/components/MainProyects.tsx
@@ -1,43 +1,46 @@
 import { favoriteProjects } from "@/lib/constant/constantProjects";
 
-
 const MainProyects = () => {
   return (
     <section className="w-4/5 my-8">
-      <h2 className="text-2xl font-bold mb-4">Proyectos Favoritos</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <h2 className="heading-primary text-center">Favorite Projects</h2>
+      <div className="flex flex-col gap-8">
         {favoriteProjects.map((project) => (
           <div
             key={project.id}
-            className="bg-backgroundLight p-4 rounded-lg transition-transform transform hover:scale-105"
+            className="card hover:card-hover flex flex-col justify-between"
           >
             <img
               src={project.image_url}
               alt={project.name}
-              className="w-full h-32 object-cover rounded-t-lg"
+              className="w-full h-40 object-cover rounded-t-card"
             />
-            <h3 className="text-xl font-semibold mt-2">{project.name}</h3>
-            <p className="text-gray-600">{project.description}</p>
-            <p className="text-sm text-gray-500">{project.language}</p>
-            <p className="mt-2">
-              <strong>Tecnologías:</strong> {project.technologies.join(", ")}
+            <h3 className="text-xl font-semibold mt-4 text-main">
+              {project.name}
+            </h3>
+            <p className="text-textSecondary mt-2">{project.description}</p>
+            <p className="text-sm text-gray-500 mt-1">
+              <strong>Language:</strong> {project.language || "Not specified"}
             </p>
-            <div className="mt-2">
+            <p className="mt-2 text-textSecondary">
+              <strong>Technologies:</strong> {project.technologies.join(", ")}
+            </p>
+            <div className="mt-4 flex gap-4">
               <a
                 href={project.html_url}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-blue-500 hover:underline mr-2"
+                className="text-primary hover:underline"
               >
-                Ver en GitHub
+                View on GitHub
               </a>
               <a
                 href={project.demo_url}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-blue-500 hover:underline"
+                className="text-primary hover:underline"
               >
-                Ver Demo
+                View Demo
               </a>
             </div>
           </div>
