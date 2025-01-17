573cb2fbc807132e9d8ac220b02803a27b4fde6c
JSSatorres <juansataz.dev@gmail.com>
2025-01-17 10:23:17 +0100

    refactor: standardize code formatting by removing unnecessary semicolons

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,43 +1,43 @@
-"use client";
+"use client"
 
-import { useEffect, useState } from "react";
-import { GitHubRepo } from "@/lib/definitions";
-import MainProyects from "./components/MainProyects";
-import GitHubProject from "./components/GitHubProjects";
+import { useEffect, useState } from "react"
+import { GitHubRepo } from "@/lib/definitions"
+import MainProyects from "./components/MainProyects"
+import GitHubProject from "./components/GitHubProjects"
 
 const ProjectsPage = () => {
-  const [repos, setRepos] = useState<GitHubRepo[]>([]);
-  const [topTechnologies, setTopTechnologies] = useState<string[]>([]);
-  const [selectedTech, setSelectedTech] = useState<string>("");
+  const [repos, setRepos] = useState<GitHubRepo[]>([])
+  const [topTechnologies, setTopTechnologies] = useState<string[]>([])
+  const [selectedTech, setSelectedTech] = useState<string>("")
 
   useEffect(() => {
     const fetchRepos = async () => {
       try {
-        const response = await fetch("/api/github");
+        const response = await fetch("/api/github")
         if (!response.ok) {
-          throw new Error("Failed to fetch repositories");
+          throw new Error("Failed to fetch repositories")
         }
 
-        const data: GitHubRepo[] = await response.json();
-        setRepos(data);
+        const data: GitHubRepo[] = await response.json()
+        setRepos(data)
 
-        const topTechnologiesFound = data.map((ele) => ele.language);
+        const topTechnologiesFound = data.map((ele) => ele.language)
         const cleanTopTechnologies = Array.from(
           new Set(topTechnologiesFound)
-        ).filter((item) => item !== null);
+        ).filter((item) => item !== null)
 
-        setTopTechnologies(cleanTopTechnologies);
+        setTopTechnologies(cleanTopTechnologies)
       } catch (error) {
-        console.error("Error fetching repositories:", error);
+        console.error("Error fetching repositories:", error)
       }
-    };
+    }
 
-    fetchRepos();
-  }, []);
+    fetchRepos()
+  }, [])
 
   const filteredRepos = repos.filter((repo) =>
     selectedTech ? repo.language === selectedTech : true
-  );
+  )
 
   return (
     <main className="min-h-screen bg-background text-main px-8 py-16 flex flex-col items-center">
@@ -49,7 +49,7 @@ const ProjectsPage = () => {
         onTechChange={(tech) => setSelectedTech(tech)}
       />
     </main>
-  );
-};
+  )
+}
 
-export default ProjectsPage;
+export default ProjectsPage
