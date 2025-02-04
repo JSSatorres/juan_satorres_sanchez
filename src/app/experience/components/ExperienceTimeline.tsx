bafaedf5fe6db80fc378bae6f97ca08d3ed545e0
JSSatorres <juansataz.dev@gmail.com>
2025-02-04 11:56:18 +0100

    fix: adjust layout and spacing in ExperienceTimeline component for improved readability

--- src/app/experience/components/ExperienceTimeline.tsx
+++ src/app/experience/components/ExperienceTimeline.tsx
@@ -13,23 +13,23 @@ const ExperienceTimeline: React.FC<YearTimelineProps> = ({
   getYearFromDate,
 }) => {
   return (
-    <div className="md:fixed md:top-50 bg-secondBackgrond p-5 flex md:flex-col flex-row md:items-center items-center justify-center card">
-      <ul className="flex md:flex-col flex-row items-center md:space-y-8 space-x-8 md:space-x-0 relative p-5">
+    <div className="md:fixed md:top-22 bg-secondBackgrond  flex md:flex-col flex-row md:items-center items-center justify-center card">
+      <ul className="flex md:flex-col flex-row items-center  md:space-y-8  md:space-x-0  ">
         {workExperience.map((work, index) => (
           <li
             key={index}
-            className={`cursor-pointer text-xl font-bold transition duration-200 text-main relative flex md:flex-row flex-col items-center`}
+            className={`cursor-pointer text-xl font-bold transition duration-200 text-main relative flex md:flex-row flex-col items-center mx-1`}
             onClick={() => handleScrollTo(index)}
           >
             <div className="relative flex items-center">
               <span
-                className={`w-4 h-4 rounded-full z-10 ${
+                className={`w-4 h-4 rounded-full z-2 ${
                   activeIndex === index
                     ? "bg-primary"
                     : "bg-white border-2 border-primary"
                 } hover:bg-opacity-75`}
               ></span>
-              <span className="text-sm ml-4 md:ml-2">
+              <span className="text-sm ml-1 md:ml-2">
                 {getYearFromDate(work?.duration)}
               </span>
             </div>
