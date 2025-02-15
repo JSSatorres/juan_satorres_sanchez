aaa9ca13503d129f86564beb9afe19dc309dd1da
JSSatorres <juansataz.dev@gmail.com>
2025-02-15 11:57:41 +0100

    fix: update Layout component to use section element for improved styling and layout

--- src/app/experience/layout.tsx
+++ src/app/experience/layout.tsx
@@ -1,3 +1,7 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
+  return (
+    <section className="md:m-7 flex flex-col md:flex-row min-h-screen bg-background text-main px-4 ">
+      {children}
+    </section>
+  )
 }
